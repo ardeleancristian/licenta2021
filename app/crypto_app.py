f8d67c727a257d4097f2d1ed1750ac453c135477
ardeleancristian <ardeleanc9@gmail.com>
2022-01-27 19:18:43 -0800

    add_new_files

---
+++ app/crypto_app.py
@@ -0,0 +1,32 @@
+
+import yfinance as yf  # Yahoo finance to get stock data
+import streamlit as st  # Streamlit to create the webapp
+from PIL import Image  # Import Pillow to add icons
+import streamlit_theme as stt
+
+from urllib.request import urlopen  # To add URLS
+stt.set_theme({'primary': '#1b3388'})
+st.write("""
+# Cryptocurrency Monitoring
+""")
+
+file=open('crypto_currencies.txt', 'r')
+ticker_list = file.read().splitlines()
+period= "6d"
+for ticker in ticker_list:
+  data=yf.Ticker(ticker)
+  data_ticker=data.history(period=period)
+  title= " """" ##  """ + ticker + " """ """"""
+  st.write(title)
+  imagePath = "static_images/"+ticker+".png"
+  image = Image.open(imagePath)
+  st.image(image, use_column_width=False)
+  st.line_chart(data_ticker.Close, use_container_width=True)
+  st.line_chart(data_ticker.Open, use_container_width=True)
+  st.line_chart(data_ticker.High, use_container_width=True)
+  st.line_chart(data_ticker.Low, use_container_width=True)
+  st.line_chart(data_ticker.Volume, use_container_width=True)
+
+
+
+
